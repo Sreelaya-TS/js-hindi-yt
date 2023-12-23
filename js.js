ee185e397dd1d4f7e351317c06fc2a5317b09cc2
Sreelaya-TS <143272867+Sreelaya-TS@users.noreply.github.com>
2023-12-23 06:02:18 +0000

    math and num

--- js.js
+++ js.js
@@ -1,21 +1,8 @@
-function echo(name, num) {
-    console.log(`Evaluating the ${name} side`);
-    return num;
-  }
-  // Exponentiation operator (**) is right-associative,
-  // but all call expressions (echo()), which have higher precedence,
-  // will be evaluated before ** does
-  console.log(echo("left", 4) ** echo("middle", 3) ** echo("right", 2));
-  // Evaluating the left side
-  // Evaluating the middle side
-  // Evaluating the right side
-  // 262144
-  
-  // Exponentiation operator (**) has higher precedence than division (/),
-  // but evaluation always starts with the left operand
-  console.log(echo("left", 4) / echo("middle", 3) ** echo("right", 2));
-  // Evaluating the left side
-  // Evaluating the middle side
-  // Evaluating the right side
-  // 0.4444444444444444
-  
+// var s = 20;
+// console.log(s)
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max - min + 1)) + min    )
